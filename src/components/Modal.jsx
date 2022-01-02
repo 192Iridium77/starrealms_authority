0005447d25215bdec5e77798b37d6f53ab2daae5
Matt Martin <matt.martin@sine.co>
2022-01-02 17:51:27 +1030

    fix modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,9 +2,9 @@ export default function Modal({ children }) {
   return (
     <>
       <div className="absolute inset-0 flex justify-center items-center z-10">
-        <div className="bg-tertiary-300 w-full lg:w-auto">
+        <div className="bg-tertiary-300">
           <div
-            className="px-8 h-screen lg:h-auto w-full lg:w-auto lg:max-h-50vh lg:min-w-600px"
+            className="px-8 lg:max-h-50vh lg:min-w-600px"
             style={{
               overflowY: "auto",
               paddingTop: "1px",
